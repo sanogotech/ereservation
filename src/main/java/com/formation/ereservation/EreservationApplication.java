1b001ef64e3e8e3bfa7a933e769c055adf6f3f9f
sanogo <sanogotech@yahoo.fr>
2019-11-20 09:51:01 +0100

    Add sample home page

---
+++ src/main/java/com/formation/ereservation/EreservationApplication.java
@@ -0,0 +1,13 @@
+package com.formation.ereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formation/ereservation/EreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.ereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
