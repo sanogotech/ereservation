5e51b43eeb64c159d2a6eef0e40d5f7de1a205ad
SANOGOTECH <sanogotech@yahoo.fr>
2019-11-28 22:08:04 +0100

    Update

--- src/test/java/com/formation/ereservation/TimeOutSampleTest.java
+++ src/test/java/com/formation/ereservation/TimeOutSampleTest.java
@@ -1,19 +1,19 @@
 package com.formation.ereservation;
 
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static java.time.Duration.ofMillis;
-import static java.time.Duration.ofMinutes;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
+import org.junit.jupiter.api.Timeout;
 
 public class TimeOutSampleTest {
 	
   //Rather than specifying timeout attribute for all tests separately, 
   //we can define JUnit Rule for all tests in a class.
   
-  @Rule
-  public Timeout globalTimeout = Timeout.seconds(5);
+ // @Rule
+ //public Timeout globalTimeout = Timeout.seconds(5);
  
   @Test
   public void simpleTest() {
@@ -21,15 +21,7 @@ public class TimeOutSampleTest {
     Assertions.assertTrue(true);
   }
   
-    //1. JUnit test timeout example – ‘timeout’ attribute
-	@Test(timeout = 500)
-	public void testInfiniteTametakingLoop() throws InterruptedException 
-	{
-		while (true)
-		{
-			Thread.currentThread().sleep(550);
-		}
-	}
+
 	
 	@Test
 	void timeoutExceeded() 
